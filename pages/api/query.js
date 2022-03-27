d559de0db3079e780bb9489fc85678e16a5c67f6
Kam Kales <kameron.kales@walgreens.com>
2022-03-26 22:30:35 -0400

    this is good enough for right now

--- pages/api/query.js
+++ pages/api/query.js
@@ -7,15 +7,22 @@ api_key.authentications["api_key"].apiKey = process.env.X_API_KEY;
 
 const data = new TransactionsApi(api_key);
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { query } = req;
   const { merchant } = query;
-  data.classifyMerchantByTransaction(merchant, function (error, data) {
+
+  data.classifyMerchantByTransaction(merchant, function (error, response) {
     if (error) {
       console.error(error);
       res.status(500).json(error);
       return;
     }
-    res.status(200).json(data);
+    console.log("response", response);
+    res.status(200).json(response);
   });
 }
+
+/* 
+  
+  });
+*/
